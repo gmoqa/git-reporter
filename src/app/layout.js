1faf3093695fc4b7595a36b03b63ba98f32bc99a
Guillermo Quinteros <guillermo.quinteros@cumplo.com>
2024-02-22 12:36:47 -0300

    feat(accesibility): update buttons and pass lighthouse tests

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,13 +1,16 @@
-'use client'
-
 import { ChakraProvider, ColorModeScript, Flex } from '@chakra-ui/react'
 import { GoogleTagManager } from '@next/third-parties/google'
 
 import Navbar from '@/app/components/Navbar'
 
+export const metadata = {
+  title: 'git reporter',
+  description: 'Gain insights from your GIT history',
+}
+
 export default function RootLayout({ children }) {
   return (
-    <html>
+    <html lang={'en'}>
       <body>
         <ColorModeScript initialColorMode={'dark'} />
         <ChakraProvider>
--- src/app/page.js
+++ src/app/page.js
@@ -20,6 +20,7 @@ import { CopyIcon } from '@chakra-ui/icons'
 import { useState } from 'react'
 import ErrorAlert from '@/app/components/alerts/ErrorAlert'
 import MarkdownPreview from '@uiw/react-markdown-preview'
+import Head from 'next/head'
 
 export default function Page() {
   const COMMAND =
@@ -121,107 +122,109 @@ export default function Page() {
   }
 
   return (
-    <Flex
-      as='main'
-      role='main'
-      direction='column'
-      flex='1'
-      px={8}
-      py={2}
-      bgColor={'gray.700'}
-    >
-      <VStack mt={4} spacing={0}>
-        <Heading as={'h1'} color={'white'}>
-          Gain insights from your GIT history
-        </Heading>
-        <Text fontSize='xl' color={'gray.400'}>
-          Unlock the power of your commit messages with Conventional Commits
-        </Text>
-        <Text color={'yellow.400'} fontSize={'sm'} fontWeight={'medium'}>
-          You are not using conventional commits?{' '}
-          <strong>
-            <a
-              href='https://www.conventionalcommits.org/en/v1.0.0/'
-              target='_blank'
-            >
-              Check this
-            </a>
-          </strong>
-        </Text>
-      </VStack>
-      <VStack pt={6} spacing={4}>
-        <VStack spacing={0}>
-          <Box p={4} bgColor={'gray.900'} borderRadius={'lg'}>
-            <HStack>
-              <Text
-                as='samp'
-                color={'gray.400'}
-                fontSize={['xs', 'sm']}
-                fontWeight={'semibold'}
+    <>
+      <Flex
+        as='main'
+        role='main'
+        direction='column'
+        flex='1'
+        px={8}
+        py={2}
+        bgColor={'gray.700'}
+      >
+        <VStack mt={4} spacing={0}>
+          <Heading as={'h1'} color={'white'}>
+            Gain insights from your GIT history
+          </Heading>
+          <Text fontSize='xl' color={'gray.400'}>
+            Unlock the power of your commit messages with Conventional Commits
+          </Text>
+          <Text color={'yellow.400'} fontSize={'sm'} fontWeight={'medium'}>
+            You are not using conventional commits?{' '}
+            <strong>
+              <a
+                href='https://www.conventionalcommits.org/en/v1.0.0/'
+                target='_blank'
               >
-                {COMMAND}
-              </Text>
-              <IconButton
-                size={'sm'}
-                borderRadius={'full'}
-                _hover={{ bgColor: 'gray.700' }}
-                color={'gray.300'}
-                bgColor={'gray.800'}
-                icon={<CopyIcon />}
-                onClick={() => onCopy()}
-              >
-                {hasCopied ? 'URL copied!' : 'Copy URL'}
-              </IconButton>
-            </HStack>
-          </Box>
-          <Text mt={2} as='i' color={'gray.400'} fontSize={['xs', 'sm']}>
-            Run this command on your proyect folder, and paste the output here
+                Check this
+              </a>
+            </strong>
           </Text>
         </VStack>
-        <Textarea
-          rows={6}
-          color={'gray.600'}
-          bgColor={'gray.200'}
-          placeholder='Paste output here'
-          onChange={(e) => setLog(e.target.value)}
-        />
-        <Button
-          isLoading={loading}
-          width={'100%'}
-          bgColor={'yellow.400'}
-          color={'gray.700'}
-          _hover={{ bgColor: 'yellow.200', color: 'gray.700' }}
-          onClick={() => handleClickRunButton()}
-        >
-          Process
-        </Button>
-      </VStack>
-      <Text mt={1} color={'gray.500'} fontSize={'xs'} textAlign={'center'}>
-        We will never store your data. This tool runs 100% on the client side.
-      </Text>
-
-      {formattedLog.length > 0 && (
-        <Box
-          data-color-mode='light'
-          mt={4}
-          bgColor={'white'}
-          p={4}
-          borderRadius={'md'}
-        >
-          <MarkdownPreview
-            source={source}
-            rehypeRewrite={(node, index, parent) => {
-              if (
-                node.tagName === 'a' &&
-                parent &&
-                /^h(1|2|3|4|5|6)/.test(parent.tagName)
-              ) {
-                parent.children = parent.children.slice(1)
-              }
-            }}
+        <VStack pt={6} spacing={4}>
+          <VStack spacing={0}>
+            <Box p={4} bgColor={'gray.900'} borderRadius={'lg'}>
+              <HStack>
+                <Text
+                  as='samp'
+                  color={'gray.400'}
+                  fontSize={['xs', 'sm']}
+                  fontWeight={'semibold'}
+                >
+                  {COMMAND}
+                </Text>
+                <Button
+                  size={'sm'}
+                  borderRadius={'md'}
+                  _hover={{ bgColor: 'gray.700' }}
+                  color={'gray.300'}
+                  bgColor={'gray.800'}
+                  leftIcon={<CopyIcon />}
+                  onClick={() => onCopy()}
+                >
+                  {hasCopied ? 'Copied!' : 'Copy'}
+                </Button>
+              </HStack>
+            </Box>
+            <Text mt={2} as='i' color={'gray.400'} fontSize={['xs', 'sm']}>
+              Run this command on your proyect folder, and paste the output here
+            </Text>
+          </VStack>
+          <Textarea
+            rows={6}
+            color={'gray.600'}
+            bgColor={'gray.200'}
+            placeholder='Paste output here'
+            onChange={(e) => setLog(e.target.value)}
           />
-        </Box>
-      )}
-    </Flex>
+          <Button
+            isLoading={loading}
+            width={'100%'}
+            bgColor={'yellow.400'}
+            color={'gray.700'}
+            _hover={{ bgColor: 'yellow.200', color: 'gray.700' }}
+            onClick={() => handleClickRunButton()}
+          >
+            Process
+          </Button>
+        </VStack>
+        <Text mt={1} color={'gray.300'} fontSize={'xs'} textAlign={'center'}>
+          We will never store your data. This tool runs 100% on the client side.
+        </Text>
+
+        {formattedLog.length > 0 && (
+          <Box
+            data-color-mode='light'
+            mt={4}
+            bgColor={'white'}
+            p={4}
+            borderRadius={'md'}
+          >
+            <MarkdownPreview
+              source={source}
+              rehypeRewrite={(node, index, parent) => {
+                if (
+                  node.tagName === 'a' &&
+                  parent &&
+                  /^h(1|2|3|4|5|6)/.test(parent.tagName)
+                ) {
+                  parent.children = parent.children.slice(1)
+                }
+              }}
+            />
+          </Box>
+        )}
+      </Flex>
+    </>
   )
 }
