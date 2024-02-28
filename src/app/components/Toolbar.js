eec6eb23a4b21f7413f952eecd96111c03768c0b
Juan Alvarado <juan.alvaradovaldivia@cencosud.cl>
2024-02-27 21:56:06 -0300

    feat(toolbar): adds filter by week, 2 weeks or month

--- src/app/components/Toolbar.js
+++ src/app/components/Toolbar.js
@@ -10,30 +10,48 @@ import {
   Spacer,
   Stack,
 } from '@chakra-ui/react'
-import { CalendarIcon, SearchIcon } from '@chakra-ui/icons'
+import { CalendarIcon } from '@chakra-ui/icons'
 import { useState } from 'react'
 import { FaFilter } from 'react-icons/fa'
+import { formatDate } from '@/app/utils/date'
 
-export default function Toolbar({ setInitialDate, setEndDate, onFilter }) {
+export default function Toolbar({
+  setInitialDate,
+  setEndDate,
+  handleFilterResults,
+}) {
   const [filter, setFilter] = useState('WEEK')
 
-  const updateFilter = (value) => {
-    setFilter(value)
+  const updateFilter = (newFilter) => {
+    const today = new Date()
+    let initialDay
+
+    if (newFilter === 'WEEK') {
+      initialDay = new Date(today).setDate(today.getDate() - 7)
+    }
+
+    if (newFilter === '2_WEEKS') {
+      initialDay = new Date(today).setDate(today.getDate() - 14)
+    }
+
+    if (newFilter === 'MONTH') {
+      initialDay = new Date(today).setMonth(today.getMonth() - 1)
+    }
+
+    handleInitialDateChange(initialDay)
+    handleEndDateChange(today)
+    setFilter(newFilter)
   }
 
-  const handleInitialDateChange = (e) => {
-    const value = e.target.value
+  const handleInitialDateChange = (value) => {
     if (value) {
-      const initialDate = new Date(value).toISOString().split('T')[0]
-      setInitialDate(initialDate)
+      setInitialDate(formatDate(value))
     }
   }
 
-  const handleEndDateChange = (e) => {
-    const value = e.target.value
+  const handleEndDateChange = (value) => {
     if (value) {
-      const endDate = new Date(value).toISOString().split('T')[0]
-      setEndDate(endDate)
+      setEndDate(formatDate(value))
     }
   }
 
@@ -63,7 +81,7 @@ export default function Toolbar({ setInitialDate, setEndDate, onFilter }) {
               borderRadius={'md'}
               borderColor={'gray.500'}
               name='initial'
-              onChange={handleInitialDateChange}
+              onChange={(e) => handleInitialDateChange(e.target.value)}
               size='sm'
               type='date'
               color='gray.400'
@@ -73,7 +91,7 @@ export default function Toolbar({ setInitialDate, setEndDate, onFilter }) {
               name='end'
               borderRadius={'md'}
               borderColor={'gray.500'}
-              onChange={handleEndDateChange}
+              onChange={(e) => handleEndDateChange(e.target.value)}
               size='sm'
               type='date'
               color='gray.400'
@@ -88,7 +106,7 @@ export default function Toolbar({ setInitialDate, setEndDate, onFilter }) {
           color={'gray.700'}
           _hover={{ bgColor: 'yellow.200', color: 'gray.700' }}
           leftIcon={<FaFilter />}
-          onClick={onFilter}
+          onClick={() => handleFilterResults(filter)}
         >
           Filter
         </Button>
